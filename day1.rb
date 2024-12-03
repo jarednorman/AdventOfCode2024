76670af7a5c1451900f022c5786c0cd9bdf5b380
Jared Norman <jared@super.gd>
2024-12-02 19:46:03 -0800

    Day 1 Part 1

--- day1.rb
+++ day1.rb
@@ -4,4 +4,16 @@ require "debug"
 
 input = File.read(File.basename(__FILE__, ".rb") + ".txt")
 
-puts input
+puts(
+  input
+    .lines
+    .map(&:strip)
+    .map { |line|
+      line.split.map { |n| n.to_i }
+    }
+    .transpose
+    .map(&:sort)
+    .transpose
+    .map { |(a, b)| (a - b).abs }
+    .sum
+)
