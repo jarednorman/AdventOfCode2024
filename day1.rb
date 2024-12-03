0144fa7ecaadc6d3feabba015b077501af25beb6
Jared Norman <jared@super.gd>
2024-12-02 19:53:41 -0800

    Day 1 Part 2

--- day1.rb
+++ day1.rb
@@ -4,16 +4,25 @@ require "debug"
 
 input = File.read(File.basename(__FILE__, ".rb") + ".txt")
 
-puts(
-  input
-    .lines
-    .map(&:strip)
-    .map { |line|
-      line.split.map { |n| n.to_i }
-    }
-    .transpose
-    .map(&:sort)
-    .transpose
-    .map { |(a, b)| (a - b).abs }
-    .sum
-)
+# puts(
+#   input
+#     .lines
+#     .map(&:strip)
+#     .map { |line|
+#       line.split.map { |n| n.to_i }
+#     }
+#     .transpose
+#     .map(&:sort)
+#     .transpose
+#     .map { |(a, b)| (a - b).abs }
+#     .sum
+# )
+
+list_one, list_two = input.lines.map(&:strip).map { |line| line.split.map { |n| n.to_i } }.transpose
+
+counts = list_two.tally
+counts.default = 0
+
+puts(list_one.sum { |n|
+  n * counts[n]
+})
