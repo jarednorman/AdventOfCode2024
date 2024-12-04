9216e07830091077e4ef8f26cd1961a73abd3e29
Jared Norman <jared@super.gd>
2024-12-04 14:36:08 -0800

    Day 3 Part 2

--- day3.rb
+++ day3.rb
@@ -4,10 +4,27 @@ require "debug"
 
 input = File.read(File.basename(__FILE__, ".rb") + ".txt")
 
-r = /mul\((\d{1,3}),(\d{1,3})\)/
+r = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/
+
+def mul(a, b)
+  return 0 unless @doing
+
+  a * b
+end
+
+@doing = true
 
 result = input.scan(r).sum { |a|
-  a.map(&:to_i).inject(:*)
+  case a
+  when "do()"
+    @doing = true
+    0
+  when "don't()"
+    @doing = false
+    0
+  else
+    eval a
+  end
 }
 
 puts result
