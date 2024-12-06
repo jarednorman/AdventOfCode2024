48bed6be47816d512f955375c1afa012f93f607c
Jared Norman <jared@super.gd>
2024-12-06 15:27:59 -0800

    Record directions of the guard as she moves

--- day6.rb
+++ day6.rb
@@ -21,43 +21,43 @@ def method_missing(method_name, *args, &block)
     @guard_direction = :up
     @x += 1
   when :guard_on_board?
-    @guard_position.all? { (0..129).cover? _1 }
+    @guard_position.first(2).all? { (0..129).cover? _1 }
   when :step
     @guard_positions ||= Set.new
     @guard_positions << @guard_position
 
     case @guard_direction
     when :up
-      new_position = [@guard_position.first, @guard_position.last - 1]
+      new_position = [@guard_position.first, @guard_position[1] - 1]
       if @obstacles[new_position]
         @guard_direction = :right
       else
-        @guard_position = new_position
+        @guard_position = [*new_position, @guard_direction]
       end
     when :right
-      new_position = [@guard_position.first + 1, @guard_position.last]
+      new_position = [@guard_position.first + 1, @guard_position[1]]
       if @obstacles[new_position]
         @guard_direction = :down
       else
-        @guard_position = new_position
+        @guard_position = [*new_position, @guard_direction]
       end
     when :down
-      new_position = [@guard_position.first, @guard_position.last + 1]
+      new_position = [@guard_position.first, @guard_position[1] + 1]
       if @obstacles[new_position]
         @guard_direction = :left
       else
-        @guard_position = new_position
+        @guard_position = [*new_position, @guard_direction]
       end
     when :left
-      new_position = [@guard_position.first - 1, @guard_position.last]
+      new_position = [@guard_position.first - 1, @guard_position[1]]
       if @obstacles[new_position]
         @guard_direction = :up
       else
-        @guard_position = new_position
+        @guard_position = [*new_position, @guard_direction]
       end
     end
   when :guard_position_count
-    @guard_positions.size
+    @guard_positions.map { _1.first(2) }.to_set.size
   when :part_one
     input.lines.map(&:strip).each { |line|
       newline
@@ -72,9 +72,14 @@ def method_missing(method_name, *args, &block)
     end
 
     guard_position_count
+  when :part_two
   else
     super
   end
 end
 
-puts part_one
+result = part_one
+raise "Part one changed to #{result}" unless result == 4656
+puts result
+
+puts part_two
