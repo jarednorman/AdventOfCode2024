668a7c5b963cb7cdfe868c7a9283670ae6ef7ddf
Jared Norman <jared@super.gd>
2024-12-13 14:34:30 -0800

    Day 9 Part 2

--- day9.rb
+++ day9.rb
@@ -4,55 +4,77 @@ require "debug"
 
 input = File.read(File.basename(__FILE__, ".rb") + ".txt").strip
 
+Chunk = Data.define(:id, :size) do
+  def free_space?
+    id.nil?
+  end
+
+  def insert(chunk)
+    raise "wtf" unless free_space?
+
+    return chunk if size == chunk.size
+
+    [
+      chunk,
+      Chunk[nil, size - chunk.size]
+    ]
+  end
+
+  def to_a
+    Array.new(size, id)
+  end
+end
+
 class Disk
   def initialize
     @mode = :file
-    @blocks = []
+    @chunks = []
     @id = 0
   end
 
   def <<(n)
     case @mode
     when :file
-      n.times do
-        @blocks << @id
-      end
+      @chunks << Chunk[@id, n]
       @id += 1
       @mode = :free_space
     when :free_space
-      n.times do
-        @blocks << nil
-      end
+      @chunks << Chunk[nil, n]
       @mode = :file
     end
   end
 
   def compact!
-    file_pointer = @blocks.length - 1
-    free_pointer = @blocks.index nil
+    file_pointer = @chunks.length - 1
 
     loop do
-      @blocks[free_pointer] = @blocks[file_pointer]
-      @blocks[file_pointer] = nil
+      chunk = @chunks[file_pointer]
+
+      free_space_pointer = @chunks.index { _1.free_space? && _1.size >= chunk.size }
+
+      if free_space_pointer && free_space_pointer < file_pointer
+        @chunks[free_space_pointer] = @chunks[free_space_pointer].insert(chunk)
+
+        @chunks[file_pointer] = Chunk[nil, chunk.size]
+        @chunks.flatten!
+      end
+
+      break if @chunks.slice(0...file_pointer).none?(&:free_space?)
 
       loop do
         file_pointer -= 1
 
-        break if @blocks[file_pointer]
+        break unless @chunks[file_pointer].free_space?
       end
-
-      loop do
-        free_pointer += 1
-
-        break if @blocks[free_pointer].nil?
-      end
-
-      break if free_pointer >= file_pointer
     end
   end
 
   def checksum
-    @blocks.each_with_index.sum { _1&.*(_2) || 0 }
+    @chunks.flat_map(&:to_a).each_with_index.sum { _1&.*(_2) || 0 }
+  end
+
+  def to_s
+    @chunks.flat_map(&:to_a).map { _1.nil? ? "." : _1.to_s }.join
   end
 end
 
@@ -64,4 +86,9 @@ end
 
 disk.compact!
 
-puts disk.checksum
+# part_one = disk.checksum
+# raise part_one.to_s unless part_one == 6398252054886
+# puts "Part One: #{part_one}"
+
+part_two = disk.checksum
+puts "Part Two: #{part_two}"
